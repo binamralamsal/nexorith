e21e1dd05c7d54ad28261fab1974a1b4238c3c87
binamralamsal <binamralamsal@gmail.com>
2024-11-08 20:02:27 +0545

    Added companies with responsiveness (was a headache), I HATE CAROUSEL

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 
+import { NexorithLogo } from "@/components/icons/logo";
+import { SquigglyLines } from "@/components/icons/squiggly-lines";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+
 import { StarIcon } from "lucide-react";
+
+const companies = [
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+  NexorithLogo,
+];
 
 export default function AboutPage() {
   return (
     <main>
-      <section className="container grid items-center gap-8 py-16 md:gap-20 md:py-20 lg:grid-cols-[3fr,2fr] lg:py-24">
+      <section className="container grid items-center gap-8 py-16 md:gap-20 md:py-20 lg:grid-cols-[3fr,2.75fr] lg:py-24">
         <div className="space-y-6 lg:space-y-8">
           <h2 className="text-balance text-4xl font-bold tracking-tighter md:max-w-[12ch] md:text-5xl lg:text-7xl">
             Exploring the Heart of Nexorith
@@ -33,14 +54,17 @@ export default function AboutPage() {
             </div>
           </div>
         </div>
-        <div className="relative flex items-center gap-6 md:gap-0 md:px-28 lg:px-0">
-          <Image
-            className="h-min w-full rounded-lg"
-            src="/about-2.jpg"
-            alt="Hero Image"
-            height={400}
-            width={400}
-          />
+        <div className="relative grid grid-cols-2 items-center gap-6 md:gap-0 md:px-28 lg:px-0">
+          <div className="relative">
+            <SquigglyLines className="absolute -left-10 -top-10 h-16 text-primary" />
+            <Image
+              className="h-min w-full rounded-lg"
+              src="/about-2.jpg"
+              alt="Hero Image"
+              height={400}
+              width={400}
+            />
+          </div>
           <div className="relative z-10 flex flex-col gap-6 md:-ml-10 md:gap-10">
             <Image
               className="h-min w-full rounded-lg object-contain"
@@ -58,6 +82,36 @@ export default function AboutPage() {
             />
           </div>
         </div>
+      </section>
+
+      <section className="container grid place-items-center gap-6 py-16 md:py-20 lg:py-24">
+        <div className="text-center font-bold">
+          Used by the world best companies
+        </div>
+        {/* <div className="flex flex-wrap justify-center gap-x-5 gap-y-6 md:gap-x-10 lg:gap-x-20">
+       
+          <NexorithLogo className="h-8" />
+          <NexorithLogo className="h-8" />
+          <NexorithLogo className="h-8" />
+          <NexorithLogo className="h-8" />
+          <NexorithLogo className="h-8" />
+        </div> */}
+
+        <Carousel
+          opts={{ align: "start", slidesToScroll: 1, loop: true }}
+          className="max-w-[90vw] md:max-w-[70w] lg:max-w-[75vw]"
+        >
+          <CarouselContent className="space-x-4">
+            {companies.map((Company, index) => (
+              <CarouselItem
+                key={index}
+                className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/6"
+              >
+                <Company className="h-[10vw] sm:h-12 md:h-10 lg:h-8" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
       </section>
     </main>
   );
